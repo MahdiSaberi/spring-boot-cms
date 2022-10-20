a253fc1f1ad4e166ef3e8f9005fbd52b1889c7b2
mahdisaberi <mahdisaberi35@gmail.com>
2022-10-20 15:12:55 +0330

    media category with swagger config created.

---
+++ src/main/java/ir/example/springbootcms/entity/MediaCategory.java
@@ -0,0 +1,43 @@
+package ir.example.springbootcms.entity;
+
+
+import ir.example.springbootcms.base.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = MediaCategory.TABLE_NAME)
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MediaCategory extends BaseEntity<Long> {
+
+    public static final String TABLE_NAME = "media_category";
+    public static final String PARENT_ID = "parent_id";
+
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = PARENT_ID)
+    private MediaCategory parent;
+
+    @OneToMany
+    @JoinColumn(name = PARENT_ID)
+//    @OneToMany(mappedBy = "parent")
+    private Set<MediaCategory> childrenCategories = new HashSet<>();
+
+    public MediaCategory(Long aLong) {
+        super(aLong);
+    }
+
+    public MediaCategory(String title) {
+        this.title = title;
+    }
+}
